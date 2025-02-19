8857166928a530280d70052a601a7477402ed56e
Sharvayu Zade <155557766+sharvayuzade@users.noreply.github.com>
2025-02-19 19:59:59 +0530

    Add files via upload
    Added magnitude values for acceleration and gyroscope readings.
    Added atan funtion formula to find tilt angle (Angle between two axes of the accelerometer sensor)
    Sensor output of following form:
    Acc. Change: total_acceleration
    Gyro Change: total_gyrochange
    Tilt Angle X: tilt_angle_X
    Tilt_Angle_Y: tilt_angle_Y

--- mpu6050_setup.ino
+++ mpu6050_setup.ino
@@ -24,22 +24,45 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  float accelX = a.acceleration.x;
+  float accelY = a.acceleration.y;
+  float accelZ = a.acceleration.z;
+  float gyroX = g.gyro.x;
+  float gyroY = g.gyro.y;
+  float gyroZ = g.gyro.z;
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+  // Serial.print("Acceleration X: ");
+  // Serial.print(accelX);
+  // Serial.print(", Y: ");
+  // Serial.print(accelY);
+  // Serial.print(", Z: ");
+  // Serial.print(accelZ);
+  // Serial.println(" m/s^2");
 
-  delay(50);
+  // Serial.print("Rotation X: ");
+  // Serial.print(gyroZ);
+  // Serial.print(", Y: ");
+  // Serial.print(gyroY);
+  // Serial.print(", Z: ");
+  // Serial.print(gyroZ);
+  // Serial.println(" rad/s");
+  float tlt_X = atan2(accelY, accelZ) * 180 / PI;
+  float tlt_Y = atan2(accelX, accelZ) * 180 / PI;
+
+  float tot_acc = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
+  float tot_gyr = sqrt(gyroX * gyroX + gyroY * gyroY + gyroZ * gyroZ);
+
+  Serial.print("Acceleration change: ");
+  Serial.print(tot_acc);
+  Serial.print("\tGyro change: ");
+  Serial.print(tot_gyr);
+
+  Serial.print("Tilt Angle Change X: ");
+  Serial.print(tlt_X);
+  Serial.print("\t Tilt Angle Change Y: ");
+  Serial.println(tlt_Y);
+
+  delay(100);
 }
+
+
